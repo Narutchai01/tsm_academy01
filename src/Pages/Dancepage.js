b05d32fda6de0b199df90ab884329fbbedd0fd28
Jenesix <acearcadia789@gmail.com>
2022-07-25 23:43:50 +0700

    pasodmaspodm

--- src/Pages/Dancepage.js
+++ src/Pages/Dancepage.js
@@ -1,7 +1,31 @@
-export default function Dancepage(){
-    return(
-        <>
-        <h1>มาโยกๆ มาโยกกัน</h1>
-        </>
+import './Dancepage.css'
+
+export default function Dancepage() {
+    return (
+        <div>
+            <div className='main-header -banner'>
+                <div className='s-container'>
+                    <div className='main-title'>
+                        <h2>About us</h2>
+                        <h5>ติดต่อเรา</h5>
+                    </div>
+                </div>
+            </div>
+            <div className='main-content'>
+            <p> TSM ACADEMY  เป็นสถาบันที่ได้นำหลักสูตรร้องเพลงและเต้น มาจาก อเมริกามาใช้ในการสอน พร้อมทั้งมีสตูดิโอให้เช่าห้องซ้อม คลาสเรียนเต้นมีตั้งแต่ Beginner Dance Intermediate Dance, Advanced Dance HipHop for kids, Girl Hip Hop  K-POP Cover, Jazz Funk, Street Dance, Hight Heels, Choreography Dance, เป็นต้น สามารถเรียนได้ตั้งแต่อายุ 3 ปี ขึ้นไป จะมีทั้งคลาสแบบหลักสูตร หรือแบบ Open Class, Private class สำหรับแก้ไขเทคนิคเฉพาะบุคคล
+                iTheme Studio แห่งนี้เป็นพื้นที่ที่ให้ทั้งเด็กและผู้ใหญ่ได้มาสนุกกับการเต้น มีสอนทุกวัน จันทร์ - ศุกร์ เวลา 4.00 - 08.00 pm เสาร์-อาทิตย์ เวลา 09.00-09.00 pm มีคุณครูผู้เชี่ยวชาญ ที่มากประสบการณ์ร่วมงานกับศิลปินมาแล้วมากมายในวงการบันเทิงและต่างประเทศ ได้มีจัดกิจกรรม Work Shop เรียนจบ ได้เต้นโชว์ความสามารถ บนเวที ที่มีการ จัดงานออดิชั่น ประกวด แข่งขัน ทุกครั้งที่มี
+            </p>
+            <br></br>
+            <h4>What you learn</h4>
+            <div className='text-learn'>
+            <li>เริ่มจากฝึกพื้นฐานการใช้กล้ามเนื้อ</li>
+            <li>การจับจังหวะเพลง</li>
+            <li>กระตุ้นความคิดสร้างสรรค์</li>
+            <li>ความกล้าแสดงออก</li>
+            <li>ส่งเสริมบุคลิกภาพ</li>
+            <li>กล้ามเนื้อยืดหยุ่นได้ดี </li>
+            </div>
+            </div>
+        </div>
     )
 }
