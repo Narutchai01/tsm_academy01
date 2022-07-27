414e61c928a6cd7e6969193716dfcdae3978e236
Narutchai <koonfluk2015@gmail.com>
2022-07-27 17:02:19 +0700

    add Card in Homepage

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,12 +1,21 @@
+import Cardhomepade from "../component/Cardhomepade"
 import Carousel from "../component/Carousel"
+import Container from "react-bootstrap/esm/Container"
+import '../style/Homepage.css'
 
 export default function Homepage(){
     return(
         
         <>
         <Carousel/>
-        <div className='App-grid'>
+        <Container>
+        <div className="Card-grid">
+            <Cardhomepade/>
+            <Cardhomepade/>
+            <Cardhomepade/>
+            <Cardhomepade/>
         </div>
+        </Container>
         </>
     )
 }
