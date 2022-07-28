d24635a89f7dad4b8d859e236ecd452c5de49b22
Narutchai <koonfluk2015@gmail.com>
2022-07-29 02:58:46 +0700

    Dancepage

---
+++ src/Data/dataDancepage.js
@@ -0,0 +1,8 @@
+const dataDance = [
+    {
+        title: "Dance",
+        text1: "TSM ACADEMY  เป็นสถาบันที่ได้นำหลักสูตรร้องเพลงและเต้น มาจาก อเมริกามาใช้ในการสอน พร้อมทั้งมีสตูดิโอให้เช่าห้องซ้อม คลาสเรียนเต้นมีตั้งแต่ Beginner Dance Intermediate Dance, Advanced Dance HipHop for kids, Girl Hip Hop  K-POP Cover, Jazz Funk, Street Dance, Hight Heels, Choreography Dance, เป็นต้น สามารถเรียนได้ตั้งแต่อายุ 3 ปี ขึ้นไป จะมีทั้งคลาสแบบหลักสูตร หรือแบบ Open Class, Private class สำหรับแก้ไขเทคนิคเฉพาะบุคคล",
+    },
+];
+
+export default dataDance;
